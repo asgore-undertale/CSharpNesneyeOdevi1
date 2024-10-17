19c1e63f0bac73458e85c78c2b6844c1474ac3aa
asgore-undertale <52295461+asgore-undertale@users.noreply.github.com>
2024-10-17 18:47:25 +0300

    Add files via upload

---
+++ Catalog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCatalog.Domain
+{
+    /// <summary>
+    /// Servisler listesini tutan, yöneten nesnedir
+    /// </summary>
+    public class Catalog
+    {
+        /*
+            Mümkün mertebe null değer kullanımından kaçınmak lazım.
+            Null check kod yönetimi ve okunurluğu açısından hatta iş kurallarının anlaşılırlığı açısından pek tercih edilmemelidir.
+
+            Aşağıdaki özellikler yine read-only tanımlanmıştır. Ancak varsayılan değerleri de set edilmiştir.
+            private set oldukları içinden nesne örneği üzerinden doğrudan değiştirilemezler ama buradaki gibi = operatörü ile ilk değerleri verilebilir.
+         */
+        /// <summary>
+        /// Servis kataloğunun adı
+        /// </summary>
+        public string Name { get; private set; } = "Default Catalog";
+        /// <summary>
+        /// Servis kataloğu ile ilgili detaylı bilgi
+        /// </summary>
+        public string Description { get; private set; } = string.Empty;
+        /// <summary>
+        /// Servislerin listesi
+        /// </summary>
+        public List<Service> Services { get; private set; } = []; // Boş bir liste oluşturur
+
+        /// <summary>
+        /// Servisleri tutan Catalog nesnesini örnekler
+        /// </summary>
+        /// <param name="name">Katalog adı</param>
+        /// <param name="description">Katalog ile ilgili genel bilgiler</param>
+        public Catalog(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        //TODO@Everyone AddService metodu nasıl daha iyi yazılabilir?
+        public void AddService(Service service)
+        {
+            if (service == null)
+            {
+                throw new Exception("Service cannot be null.");
+            }
+
+            if (Services.Contains(service))
+            {
+                return;
+            }
+
+            Services.Add(service);
+        }
+        //public void AddService(string alias,IPAddress address,short port)
+        //{
+
+        //}
+        //public void AddService(string alias, IPAddress address)
+        //{
+
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceCatalog.Domain;
+using System.Net;
+
+namespace ServiceCatalog
+{
+    /*
+        Main metodu olan sınıf aslında bir Executable çıktının çalışma zamanında giriş noktasıdır (EntryPoint)
+        internal erişim belirleyicisi sadece bu Assembly(Executable) içerisinde kullanılabilir anlamına gelir.
+        internal dışında public, private, protected gibi farklı Access Modifier'lar da vardır.
+
+        Main genelde bir değer döndürmez ama bazı durumlarda int gibi dönüşleri olabilir.
+
+        Varsayılan durumda(Default) tüm türler bir namespace içerisinde yer alır.
+
+        Senaryomuzda servisleri tanımlayan bir sınıfımız(nesne tasarımı) olacak.
+        Bunların listesini tutan da bir container yapısı kullanacağız.
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Servis nesnesini aşağıdaki gibi default constructor veya parametreli private yapılmış versiyonu ile çalıştıramayız.
+            // Service redisService = new Service()
+
+            //TODO@Everyone Create fonksiyonundan geriye null değer dönsün istemiyorum. Kodu okurken daha anlaşılır olmasını da istiyorum. Ne yaparsınız?
+            var redisService = Service.Create("Redis Service", new IPAddress([0, 0, 0, 0]), 6379);
+            if (redisService == null)
+            {
+                throw new Exception("Service Error: Service was not created successfully.");
+            }
+        }
+    }
+}
